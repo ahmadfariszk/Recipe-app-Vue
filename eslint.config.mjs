1596089f15f50b1098577467bf2339e4c451e467
Ahmad Faris <149571714+ahmadfariszk@users.noreply.github.com>
2024-11-08 15:10:41 +0800

    Installed the linter gang
    - added eslint
    - added prettier
    - added husky lint-stage

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,18 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default withNuxt(
   // Your custom configs here
+  {
+    files: ['**/*.js', '**/*.vue'],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettierConfig.rules,          // Import Prettier rules
+      'prettier/prettier': 'error',     // Flag Prettier issues as ESLint errors
+    },
+  }
 )
