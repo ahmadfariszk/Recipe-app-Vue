e2ee940e06df9df5723deeb5407f5910e2bfd094
Ahmad Faris <149571714+ahmadfariszk@users.noreply.github.com>
2024-11-10 03:11:16 +0800

    Added search component

---
+++ components/search/SearchHeader.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex justify-center items-center min-h-12 pt-8 -mb-12 relative bg-blue-500"></div>
+  <div class="flex justify-center items-center pt-8">
+      <FloatLabel variant="over">
+        <IconField >
+            <InputIcon class="pi pi-search" >A</InputIcon>
+            <InputText id="over_label" class="h-12 min-w-72 " autocomplete="off" />
+        </IconField>
+        <label for="over_label">Over Label</label>
+      </FloatLabel>
+  </div>
+</template>
--- components/search/SearchPage.vue
+++ components/search/SearchPage.vue
@@ -1,3 +1,4 @@
 <template>
+  <SearchHeader/>
   <RecipeList/>
 </template>
