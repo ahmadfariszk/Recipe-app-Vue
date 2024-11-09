f9cbb81768797218bd309d000923e365080d7e52
Ahmad Faris <149571714+ahmadfariszk@users.noreply.github.com>
2024-11-09 22:40:15 +0800

    Fixed type issue

--- components/recipe/RecipeIntructions.vue
+++ components/recipe/RecipeIntructions.vue
@@ -1,24 +1,30 @@
 <template>
-  <div class="flex" v-for="(instruction, step) in recipeInstructions" :key="step">
+  <div
+    class="flex"
+    v-for="(instruction, step) in recipeInstructions"
+    :key="step"
+  >
     <div>
       <div
         class="flex items-center justify-center rounded-full bg-slate-600 w-12 h-12 text-white text-center mx-2"
       >
-        {{step+1}}
+        {{ step + 1 }}
       </div>
     </div>
     <div>
-      {{ instruction }}
+      {{ instruction.text }}
     </div>
   </div>
 </template>
 <script lang="ts">
-  export default {
-    props: {
-      recipeInstructions: {
-        type: Array,
-        required: true
-      }
-    }
-  }
-  </script>
+interface Instruction {
+  text: string;
+}
+
+const props = defineProps({
+  recipeInstructions: {
+    type: Array as PropType<Instruction[]>,
+    required: true,
+  },
+});
+</script>
