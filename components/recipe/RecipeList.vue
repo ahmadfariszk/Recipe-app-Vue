0bed996aa9a6af7b0eeba3c3d1a3d85882e70501
Ahmad Faris <149571714+ahmadfariszk@users.noreply.github.com>
2024-11-10 01:09:45 +0800

    Refactored elements on search page
    - into RecipeList

---
+++ components/recipe/RecipeList.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- Check if recipeArray is defined and has items before rendering -->
+    <div class=" p-4">
+      <div v-if="recipeArray && recipeArray.length">
+        <RecipeCardHorizontal
+          v-for="recipe in recipeArray"
+          :key="recipe.name"
+          :recipe="recipe"
+        />
+      </div>
+      <div v-else>
+        <p>Loading recipes...</p>
+      </div>
+    </div>
+  </template>
+  <script setup>
+  import { computed } from 'vue';
+  import { useRecipeArrayStore } from '@/stores/recipeArrayStore';
+  
+  const recipeStore = useRecipeArrayStore();
+  console.log(recipeStore.getRecipeArray)
+  
+  const recipeArray = computed(() => recipeStore.getRecipeArray);
+  </script>
