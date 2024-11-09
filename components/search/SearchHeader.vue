ec8fad8ae1e1766952e1e258fc1a5071cbac8f09
Ahmad Faris <149571714+ahmadfariszk@users.noreply.github.com>
2024-11-10 03:23:25 +0800

    Added lucide library

--- components/search/SearchHeader.vue
+++ components/search/SearchHeader.vue
@@ -3,10 +3,13 @@
   <div class="flex justify-center items-center pt-8">
       <FloatLabel variant="over">
         <IconField >
-            <InputIcon class="pi pi-search" >A</InputIcon>
+            <InputIcon class="pi pi-search" ><Search :size="20" /></InputIcon>
             <InputText id="over_label" class="h-12 min-w-72 " autocomplete="off" />
         </IconField>
-        <label for="over_label">Over Label</label>
+        <label for="over_label" >Over Label</label>
       </FloatLabel>
   </div>
 </template>
+<script setup lang="ts">
+import { Search } from 'lucide-vue-next';
+</script>
