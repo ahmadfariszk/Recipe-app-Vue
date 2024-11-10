e827e3d2e40c476e27eff8faaa64290083f92ab7
Ahmad Faris <149571714+ahmadfariszk@users.noreply.github.com>
2024-11-10 20:39:15 +0800

    Fixed toggle bugs

--- components/favourite/FavouriteToggleButton.vue
+++ components/favourite/FavouriteToggleButton.vue
@@ -2,14 +2,15 @@
 import { useRecipeArrayStore } from '~/stores/recipeArrayStore';
 import { computed } from 'vue';
 
-const props = defineProps<{ recipe: { id: number; [key: string]: any } }>();
+const props = defineProps<{ recipeId: number }>();
 const recipeStore = useRecipeArrayStore();
 
 // Define a computed property for `v-model`
 const isFavorite = computed({
-  get: () => recipeStore.favRecipeArray.includes(props.recipe.id),
+  get: () => recipeStore.favRecipeArray.includes(props.recipeId),
   set: (value) => {
-    recipeStore.toggleFavorite(props.recipe.id);
+    console.log('props', props.recipeId)
+    recipeStore.toggleFavorite(props.recipeId);
   }
 });
 </script>
@@ -23,5 +24,8 @@ const isFavorite = computed({
     offIcon="pi pi-heart"
     size="small"
     class="rounded-full max-w-6 max-h-6 border-surface-300 bg-transparent data-[p-checked=true]:bg-surface-200 data-[p-checked=true]:before:bg-surface-200 data-[p-checked=true]:before:shadow-none"
+    :pt="{
+      content: 'gap-0'
+    }"
   />
 </template>
