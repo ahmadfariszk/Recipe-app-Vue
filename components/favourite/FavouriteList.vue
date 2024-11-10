5b47c2d3af70fc26ba3d10104e2ceb365050f96b
Ahmad Faris <149571714+ahmadfariszk@users.noreply.github.com>
2024-11-11 04:17:05 +0800

    Added tips to home page

--- components/favourite/FavouriteList.vue
+++ components/favourite/FavouriteList.vue
@@ -2,22 +2,27 @@
 const recipeStore = useRecipeArrayStore();
 const favRecipeArray = computed(() => recipeStore.getFavRecipeArray);
 recipeStore.listenLocalStorageEventUpdate();
-
 </script>
 
 <template>
-    <div class="p-4">
-      <div class="title text-xl">Your Favourites</div>
-      <div v-if="favRecipeArray && favRecipeArray?.length">
-        <RecipeCardHorizontal
-          v-for="recipe in favRecipeArray"
-          :key="recipe?.name"
-          :recipe="recipe"
-          @click="handleClickToRecipe(recipe?.id)"
-        />
-      </div>
-      <div v-else>
-        <p>Loading recipes...</p>
-      </div>
+  <div class="p-4">
+    <div class="title text-2xl font-semibold">Your Favourites</div>
+    <div class="title text-sm flex items-center gap-1 text-muted-color">
+      <i class="pi pi-info-circle" style="font-size: 0.75rem"></i>
+      Remove from favourites by toggling 
+      <i class="pi pi-heart-fill" style="font-size: 0.75rem"></i>
+      button
     </div>
+    <div v-if="favRecipeArray && favRecipeArray?.length">
+      <RecipeCardHorizontal
+        v-for="recipe in favRecipeArray"
+        :key="recipe?.name"
+        :recipe="recipe"
+        @click="handleClickToRecipe(recipe?.id)"
+      />
+    </div>
+    <div v-else>
+      <p>Loading recipes...</p>
+    </div>
+  </div>
 </template>
